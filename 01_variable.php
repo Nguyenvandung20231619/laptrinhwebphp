b6e8fdac3c66b60f391240ea6d9c16ce47c90613
Nguyenvandung20231619 <nvd7125@gmail.com>
2025-12-31 09:19:18 +0700

    Add files via upload

---
+++ 01_variable.php
@@ -0,0 +1,12 @@
+<?php
+$Ten = "Nguyễn Văn Dũng";
+$Lop = "DCCNTT14.4";
+$Email = "20231619@eaut.edu.vn";
+
+echo "<h3>Thông tin cá nhân</h3>";
+echo "<ul>";
+echo "<li>Họ và tên: $Ten</li>";
+echo "<li>Lớp: $Lop</li>";
+echo "<li>Email: $Email</li>";
+echo "</ul>";
+?>
---
+++ 02_operators_math.php
@@ -0,0 +1,9 @@
+<?php
+$bankinh = 3;
+$pi = 3.14;
+$Chu_vi =2*$pi*$bankinh;
+$Dien_tich =$pi*$Chu_vi*$Chu_vi;
+echo "<h3>kết quả</h3>";
+echo "Chu vi: ".$Chu_vi."<br>";
+echo "Diện tích: ".$Dien_tich."<br>";
+?>
